558471feb5c46f0221d721797db5bcffa4ec5cbd
Lenard Howell <len.howell@gmail.com>
2023-03-26 09:34:59 -0400

    Initial Commit

---
+++ icloudPlayGround/icloudPlayGroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  icloudPlayGroundApp.swift
+//  icloudPlayGround
+//
+//  Created by Lenard Howell on 3/26/23.
+//
+
+import SwiftUI
+
+@main
+struct icloudPlayGroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
